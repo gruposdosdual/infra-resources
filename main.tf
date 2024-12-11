b5fb61e01cc302de44b9c5c9a2125316a5987a47
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2024-12-11 18:38:03 +0100

    add main

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 provider "aws" {
-  region  = "us-west-2"
-  profile = "EKS-alumnos-248189943700"
+  region = "eu-west-3"
+ }
+  
+
+resource "aws_instance" "practica_proveedor" {
+  ami           = "ami-01a56e5a2f9ca07ca"
+  instance_type = "t2.micro"
+  tags = {
+    Name = var.nombre_instancia
+  }
 }
