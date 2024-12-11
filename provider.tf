ed35d74d8054b8ecc27f7d92a73b1a61ca4874fd
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2024-12-11 16:28:51 +0100

    practica terraform

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
+
 provider "local"{}
 
 resource "local_file" "ejercicio-1"{
     filename = "practicas-proveedor.txt"
     content = "¡Hola, Terraform!"
 }
-=======
+
 provider "local" {
   
 }
@@ -15,4 +15,4 @@ resource "local_file" "ejercicio-1" {
   content = "Hola, Terraform!"
 }
 
->>>>>>> 7a1a2cb0ab3bb89a3e9f01aa0855354c414c5f19
+
