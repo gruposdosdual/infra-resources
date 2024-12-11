6e4fe77744dafc93601e66d56bd0a7bc3f963da9
Jorge382 <jorge.vidal@campusdual.com>
2024-12-11 18:55:44 +0100

    Eliminado

--- vm.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "eu-west-3" # Datacenter en París
-}
-
-resource "aws_instance" "practica_proveedor" {
-  ami           = "ami-01a56e5a2f9ca07ca"
-  instance_type = "t2.micro"
-}
