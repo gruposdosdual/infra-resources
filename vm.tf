e3094fc8e1c55fe63b298c5d5c645eb749887e6c
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2024-12-11 16:41:14 +0100

    practica terraform

--- vm.tf
+++ vm.tf
@@ -1,7 +1,8 @@
-provider "aws"{
-    region = "eu-west-3" #datacenter en Paris
+provider "aws" {
+  region = "eu-west-3" # Datacenter en París
 }
-resource = "aws_instance" "practica-proveedor"{
-    ami = "ami-01a56e5a2f9ca07ca"
-    instance_type = "t2.micro"
+
+resource "aws_instance" "practica_proveedor" {
+  ami           = "ami-01a56e5a2f9ca07ca"
+  instance_type = "t2.micro"
 }
